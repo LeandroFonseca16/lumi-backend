112ec65cef552bf5691c931173144341f2625130
Bruno Brito <xbrunots@gmail.com>
2024-10-20 12:38:39 -0300

    !o

--- src/routers/chart_router.ts
+++ src/routers/chart_router.ts
@@ -3,5 +3,5 @@ import chart_controller from '../controllers/chart_controller';
 
 const router = express.Router();
 
-router.get('/:customerId', chart_controller.getChart);
+router.get('/', chart_controller.getChart);
 export default router;
